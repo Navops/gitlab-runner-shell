19c5b6c09a61db85f1e2eae7eab3d9ab4e9311ca
Cameron Brunner <brunner@univa.com>
2016-06-29 13:05:06 -0500

    Fix typo in entrypoint

--- entry_point.sh
+++ entry_point.sh
@@ -7,8 +7,8 @@ DOCKER_ID=`ls -ln /var/run/docker.sock | awk '{ print $4 }'`
 
 GROUP_NAME=`grep ":$DOCKER_ID:" /etc/group | sed 's|:.*||' 2>/dev/null`
 if [ -z "$GROUP_NAME" ]; then
-   $GROUP_NAME=docker
-   groupadd -g $DOCKER_ID $GROUP_NAME
+  GROUP_NAME=docker
+  groupadd -g $DOCKER_ID $GROUP_NAME
 fi
 
 usermod -a -G $GROUP_NAME gitlab-runner 
